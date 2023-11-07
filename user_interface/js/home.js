50722d065916118cd3fd6a9764edcf4f88abfff2
carmelaroth <146923908+carmelaroth@users.noreply.github.com>
2023-11-07 12:44:12 +0100

    added login window

--- user_interface/js/home.js
+++ user_interface/js/home.js
@@ -6,3 +6,19 @@ $(document).ready(function() {
     updateNetInfo();
     setInterval(updateNetInfo, 10000);
 });
+
+const loginButton = document.getElementById("loginButton");
+const loginContainer = document.getElementById("loginContainer");
+
+loginButton.addEventListener("click", () => {
+    loginContainer.style.display = "block";
+});
+
+const submitLogin = document.getElementById("submitLogin");
+
+submitLogin.addEventListener("click", () => {
+    // Hier können Sie die Logik für den Login-Vorgang implementieren
+    // Zum Beispiel: Überprüfen Sie Benutzername und Passwort
+    alert("login successful!");
+    loginContainer.style.display = "none";
+});
