924097a590df082b46b3647c0cfe19d1d65f96b6
Colin Wallimann <colin.wallimann@protonmail.com>
2023-11-14 09:44:45 +0100

    renamed files

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -1,28 +1,31 @@
-CREATE TABLE gta23.coverage_map(
-    coverage_id SERIAL PRIMARY KEY,
-    coverage BOOLEAN,
-    net_speed VARCHAR(16),
-    provider VARCHAR(16),
-    geom POINT);
+CREATE TABLE antenna_locations(
+     antenna_id SERIAL PRIMARY KEY,
+     radiated_power VARCHAR(16),
+     geom POINT);
 
-CREATE TABLE gta23.antenna_locations(
-    antenna_id SERIAL PRIMARY KEY,
-    radiated_power VARCHAR(16),
-    geom POINT);
+CREATE TABLE train_tracks(
+     track_id SERIAL PRIMARY KEY,
+     geom geometry);
 
-CREATE TABLE gta23.train_tracks(
-    track_id SERIAL PRIMARY KEY,
-    geom POINT);
 
-CREATE TABLE gta23.user(
-    user_id SERIAL PRIMARY KEY,
-    name VARCHAR(16),
-    passwort CHAR(256),
-    provider VARCHAR(16));
+CREATE TABLE user(
+     user_id SERIAL PRIMARY KEY,
+     name VARCHAR(16),
+     password CHAR(256),
+     provider VARCHAR(16));
 
-CREATE TABLE gta23.user_data(
-    data_id SERIAL PRIMARY KEY,
-    user_id INTEGER references gta23.user(user_id),
-    net_speed VARCHAR(16),
-    geom POINT,
-    provider VARCHAR(16));
+CREATE TABLE user_trajectory_data(
+     user_trajectory_id SERIAL PRIMARY KEY,
+     distance REAL,
+     time TIMESTAMP,
+     user_id INTEGER references user(user_id),
+     geom geometry);
+
+
+CREATE TABLE user_point_data(
+     data_id SERIAL PRIMARY KEY,
+     netspeed VARCHAR(16),
+     geom POINT,
+     provider VARCHAR(16),
+     time TIMESTAMP,
+     user_tractory_id INTEGER references user_trajectory_data(user_trajectory_id));
