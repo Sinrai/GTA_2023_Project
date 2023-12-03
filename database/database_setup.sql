d92e5ac5587d7970ee52ba5cbcc245c4bdea5c1c
jiatian333 <146749786+jiatian333@users.noreply.github.com>
2023-12-03 13:30:40 +0100

    Modified database with trajectory id

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -4,28 +4,28 @@ DROP TABLE IF EXISTS gta_p4.user_trajectory_data;
 DROP TABLE IF EXISTS gta_p4.user_point_data;
 
 CREATE TABLE gta_p4.antenna_locations(
-    antenna_id SERIAL PRIMARY KEY,
-    radiated_power VARCHAR(16),
-    type SMALLINT);
+     antenna_id SERIAL PRIMARY KEY,
+     radiated_power VARCHAR(16),
+     type INTEGER,
+     geom GEOMETRY);
 
 CREATE TABLE gta_p4.train_tracks(
-    track_id SERIAL PRIMARY KEY);
+     track_id SERIAL PRIMARY KEY,
+     geom GEOMETRY);
 
 CREATE TABLE gta_p4.user_trajectory_data(
-    user_trajectory_id SERIAL PRIMARY KEY,
-    distance REAL,
-    time TIMESTAMP,
-    username VARCHAR(255));
+     user_trajectory_id SERIAL PRIMARY KEY,
+     distance REAL,
+     time TIMESTAMP,
+     username VARCHAR(255),
+     geom GEOMETRY);
 
 CREATE TABLE gta_p4.user_point_data(
-    data_id SERIAL PRIMARY KEY,
-    netspeed SMALLINT,
-    ip VARCHAR(255),
-    provider VARCHAR(255),
-    time TIMESTAMP,
-    username VARCHAR(255));
-
-SELECT AddGeometryColumn('gta_p4', 'antenna_locations', 'geom', '2056', 'POINT', 2);
-SELECT AddGeometryColumn('gta_p4', 'train_tracks', 'geom', '2056', 'LINESTRING', 2);
-SELECT AddGeometryColumn('gta_p4', 'user_trajectory_data', 'geom', '4326', 'LINESTRING', 2);
-SELECT AddGeometryColumn('gta_p4', 'user_point_data', 'geom', '4326', 'POINT', 2);
+     data_id SERIAL PRIMARY KEY,
+     netspeed VARCHAR(16),
+     provider VARCHAR(16),
+     time TIMESTAMP,
+     username VARCHAR(255),
+     geom GEOMETRY,
+     in_train BOOLEAN,
+     user_trajectory_id INTEGER REFERENCES gta_p4.user_trajectory_data(user_trajectory_id);)
